db25175470346e65823e33cac818dfb9cd17857f
Le Trung Kien <kientrungle2001@gmail.com>
2019-12-29 21:36:32 +0700

    dang nhap

--- application/views/anphat/default/nhan_vien/dang_nhap.php
+++ application/views/anphat/default/nhan_vien/dang_nhap.php
@@ -1,20 +1,20 @@
 <div class="container">
     <div class="card card-login mx-auto text-center">
         <div class="card-header mx-auto">
-            <span class="logo_title mt-5"> Login Dashboard </span>
+            <span class="logo_title mt-5"> Đăng nhập </span>
         </div>
         <div class="card-body">
             <form action="" method="post">
                 <div class="input-group form-group">
                     <div class="input-group-prepend">
-                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                        <span class="input-group-text"><i class="fa fa-user"></i></span>
                     </div>
                     <input type="text" name="email" class="form-control" placeholder="Username">
                 </div>
 
                 <div class="input-group form-group">
                     <div class="input-group-prepend">
-                        <span class="input-group-text"><i class="fas fa-key"></i></span>
+                        <span class="input-group-text"><i class="fa fa-key"></i></span>
                     </div>
                     <input type="password" name="password" class="form-control" placeholder="Password">
                 </div>
