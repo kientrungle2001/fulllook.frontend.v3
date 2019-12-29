95005a619298b2a7c09e08ddbe10c70111949630
Le Trung Kien <kientrungle2001@gmail.com>
2019-12-29 22:33:06 +0700

    dang nhap

--- application/views/anphat/default/nhan_vien/dang_nhap.php
+++ application/views/anphat/default/nhan_vien/dang_nhap.php
@@ -9,18 +9,18 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fa fa-user"></i></span>
                     </div>
-                    <input type="text" name="email" class="form-control" placeholder="Username">
+                    <input type="text" ng-model="ten_dang_nhap" class="form-control" placeholder="Tên đăng nhập">
                 </div>
 
                 <div class="input-group form-group">
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fa fa-key"></i></span>
                     </div>
-                    <input type="password" name="password" class="form-control" placeholder="Password">
+                    <input type="password" ng-model="mat_khau" class="form-control" placeholder="Mật khẩu">
                 </div>
 
                 <div class="form-group">
-                    <input type="submit" name="btn" value="Login" class="btn btn-outline-danger float-right login_btn">
+                    <input type="submit" name="btn" value="Đăng nhập" class="btn btn-outline-danger float-right login_btn">
                 </div>
 
             </form>
