801baa339b86c734cd4c9d88437060980e3f5bf2
Le Trung Kien <kientrungle2001@gmail.com>
2020-01-04 15:03:21 +0700

    class student date

--- application/views/qlhs/default/student/detail/view/advice.php
+++
@@ -1,5 +0,0 @@
-<h1>Tư vấn <button class="btn btn-primary" 
-  onclick="jQuery('#modal_advice').modal('show')">Thêm</button></h1>
-<?php $controller->view('student/detail/view/advice/list')?>
-<?php $controller->view('student/detail/view/advice/add')?>
-<?php $controller->view('student/detail/view/advice/edit')?>
--- application/views/qlhs/default/student/detail/view/fee.php
+++
@@ -1,4 +0,0 @@
-<h1>Học phí <button class="btn btn-primary" 
-  onclick="jQuery('#modal_fee').modal('show')">Thêm</button></h1>
-<?php $c->view('student/detail/view/fee/add')?>
-<?php $c->view('student/detail/view/fee/list')?>
