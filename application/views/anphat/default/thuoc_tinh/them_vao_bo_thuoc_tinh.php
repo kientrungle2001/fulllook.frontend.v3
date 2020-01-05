532b9aee7ca4a3bfb4591c22576add85f13bfced
Le Trung Kien <kientrungle2001@gmail.com>
2020-01-05 15:27:15 +0700

    them thuoc tinh vao bo thuoc tinh

--- application/views/anphat/default/thuoc_tinh/them_vao_bo_thuoc_tinh.php
+++ application/views/anphat/default/thuoc_tinh/them_vao_bo_thuoc_tinh.php
@@ -1,3 +1,8 @@
-<div>
-  <h1>Thuoc tinh <?= $ten_thuoc_tinh?> <?= $_id['$oid']?></h1>
+<div class="container-fluid">
+  <h1>Thuoc tinh: <?= $ten_thuoc_tinh?> - <?= $_id['$oid']?></h1>
+  <select ng-model="id_bo_thuoc_tinh" class="btn btn-primary">
+    <option>Chon bo thuoc tinh</option>
+    <option ng-repeat="bo_thuoc_tinh in danh_sach_bo_thuoc_tinh" ng-value="bo_thuoc_tinh._id.$oid">{{bo_thuoc_tinh.ten_bo_thuoc_tinh}}</option>
+  </select>
+  <button ng-click="them_vao_bo_thuoc_tinh()">Them vao bo thuoc tinh</button>
 </div>
