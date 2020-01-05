07e97b5bb9356e45b7f4f101a35a87e385829d03
Le Trung Kien <kientrungle2001@gmail.com>
2020-01-05 13:34:03 +0700

    phan trang len dau

--- application/views/anphat/default/tong_quat/danh_sach.php
+++ application/views/anphat/default/tong_quat/danh_sach.php
@@ -1,4 +1,4 @@
-
+<?php $c->view('tong_quat/phan_trang');?>
 <table class="table table-sm table-bordered">
   <tr>
   <th><input type="checkbox" ng-model="trang_thai_chon_tat_ca" ng-change="chon_tat_ca()" /></th>
@@ -29,17 +29,4 @@
   </tr>
 </table>
 
-<nav aria-label="Page navigation example">
-  <ul class="pagination">
-  <li class="page-item">Kích cỡ trang: <select class="btn btn-primary btn-sm" ng-model="phan_trang.kich_co_trang" ng-change="tai_danh_sach()">
-    <option ng-value="10">10</option>
-    <option ng-value="20">20</option>
-    <option ng-value="50">50</option>
-    <option ng-value="100">100</option>
-    <option ng-value="100">200</option>
-  </select></li>
-    <li class="page-item"><a class="page-link" href="#" onclick="return false" ng-click="den_trang_truoc()">Trang trước</a></li>
-    <li class="page-item"><a class="page-link" href="#" onclick="return false">{{phan_trang.trang_hien_thoi + 1}}</a></li>
-    <li class="page-item"><a class="page-link" href="#" onclick="return false" ng-click="den_trang_tiep()">Trang tiếp</a></li>
-  </ul>
-</nav>
+<?php $c->view('tong_quat/phan_trang');?>
