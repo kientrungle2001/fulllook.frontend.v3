5a5d15b6aeb918e5325b1abff0d3d4eb79f63199
kienlt <kientrungle2001@gmail.com>
2020-05-10 22:48:32 +0700

    change route

--- application/config/routes.php
+++ application/config/routes.php
@@ -69,43 +69,6 @@ if($_SERVER['HTTP_HOST'] == 'pql.vn' || $_SERVER['HTTP_HOST'] == 'pql.nn-center.
 	$route['([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)/[\w\d\-_]+\-p(:num).html'] = 'product/detail_slug/vi/$3/$4/$1/$2';
 	$route['en/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)/[\w\d\-_]+\-p(:num).html'] = 'product/detail_slug/en/$3/$4/$1/$2';
 
-	# category
-	$route['san-pham/[\w\d\-_]+\-c(:num)'] = 'product/category/vi/$1';
-	$route['en/san-pham/[\w\d\-_]+\-c(:num)'] = 'product/category/en/$1';
-	$route['san-pham/[\w\d\-_]+\-c(:num)/[\w\d\-_]+\-c(:num)'] = 'product/category/vi/$1/$2';
-	$route['en/san-pham/[\w\d\-_]+\-c(:num)/[\w\d\-_]+\-c(:num)'] = 'product/category/en/$1/$2';
-	$route['san-pham/[\w\d\-_]+\-c(:num)/[\w\d\-_]+\-c(:num)/[\w\d\-_]+\-c(:num)'] = 'product/category/vi/$1/$2/$3';
-	$route['en/san-pham/[\w\d\-_]+\-c(:num)/[\w\d\-_]+\-c(:num)/[\w\d\-_]+\-c(:num)'] = 'product/category/en/$1/$2/$3';
-	# short
-	$route['[\w\d\-_]+\-cp(:num)'] = 'product/category/vi/$1';
-	$route['en/[\w\d\-_]+\-cp(:num)'] = 'product/category/en/$1';
-	# category without id
-	$route['([\w\d\-_]+)'] = 'product/category_slug/vi/$1';
-	$route['en/([\w\d\-_]+)'] = 'product/category_slug/en/$1';
-	$route['([\w\d\-_]+)/([\w\d\-_]+)'] = 'product/category_slug/vi/$2/$1';
-	$route['en/([\w\d\-_]+)/([\w\d\-_]+)'] = 'product/category_slug/en/$2/$1';
-	$route['([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)'] = 'product/category_slug/vi/$3/$2/$1';
-	$route['en/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)'] = 'product/category_slug/en/$3/$2/$1';
-	$route['([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)'] = 'product/category_slug/vi/$4/$3/$2/$1';
-	$route['en/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)'] = 'product/category_slug/en/$4/$3/$2/$1';
-	# category without id with html
-	$route['([\w\d\-_]+)\.html'] = 'product/category_slug/vi/$1';
-	$route['en/([\w\d\-_]+)\.html'] = 'product/category_slug/en/$1';
-	$route['([\w\d\-_]+)/([\w\d\-_]+)\.html'] = 'product/category_slug/vi/$2/$1';
-	$route['en/([\w\d\-_]+)/([\w\d\-_]+)\.html'] = 'product/category_slug/en/$2/$1';
-	$route['([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)\.html'] = 'product/category_slug/vi/$3/$2/$1';
-	$route['en/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)\.html'] = 'product/category_slug/en/$3/$2/$1';
-	$route['([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)\.html'] = 'product/category_slug/vi/$4/$3/$2/$1';
-	$route['en/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)\.html'] = 'product/category_slug/en/$4/$3/$2/$1';
-	# short with html
-	$route['[\w\d\-_]+\-cp(:num).html'] = 'product/category/vi/$1';
-	$route['en/[\w\d\-_]+\-cp(:num).html'] = 'product/category/en/$1';
-	$route['[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num).html'] = 'product/category/vi/$2/$1';
-	$route['en/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num).html'] = 'product/category/en/$2/$1';
-	$route['[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num).html'] = 'product/category/vi/$3/$2/$1';
-	$route['en/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num).html'] = 'product/category/en/$3/$2/$1';
-	$route['[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num).html'] = 'product/category/vi/$4/$3/$2/$1';
-	$route['en/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num).html'] = 'product/category/en/$4/$3/$2/$1';
 	
 	# product
 	$route['san-pham/[\w\d\-_]+\-c(:num)/[\w\d\-_]+\-p(:num).html'] = 'product/detail/vi/$1/$2';
@@ -166,5 +129,45 @@ if($_SERVER['HTTP_HOST'] == 'pql.vn' || $_SERVER['HTTP_HOST'] == 'pql.nn-center.
 	$route['en/tim-kiem'] = 'search/result/en';
 	$route['tim-kiem/san-pham'] = 'search/product/vi';
 	$route['en/tim-kiem/san-pham'] = 'search/product/en';
+	$route['cart'] = 'cart/index/vi';
 	$route['en/cart'] = 'cart/index/en';
+
+	# category
+	$route['san-pham/[\w\d\-_]+\-c(:num)'] = 'product/category/vi/$1';
+	$route['en/san-pham/[\w\d\-_]+\-c(:num)'] = 'product/category/en/$1';
+	$route['san-pham/[\w\d\-_]+\-c(:num)/[\w\d\-_]+\-c(:num)'] = 'product/category/vi/$1/$2';
+	$route['en/san-pham/[\w\d\-_]+\-c(:num)/[\w\d\-_]+\-c(:num)'] = 'product/category/en/$1/$2';
+	$route['san-pham/[\w\d\-_]+\-c(:num)/[\w\d\-_]+\-c(:num)/[\w\d\-_]+\-c(:num)'] = 'product/category/vi/$1/$2/$3';
+	$route['en/san-pham/[\w\d\-_]+\-c(:num)/[\w\d\-_]+\-c(:num)/[\w\d\-_]+\-c(:num)'] = 'product/category/en/$1/$2/$3';
+	# short
+	$route['[\w\d\-_]+\-cp(:num)'] = 'product/category/vi/$1';
+	$route['en/[\w\d\-_]+\-cp(:num)'] = 'product/category/en/$1';
+	# category without id
+	$route['([\w\d\-_]+)'] = 'product/category_slug/vi/$1';
+	$route['en/([\w\d\-_]+)'] = 'product/category_slug/en/$1';
+	$route['([\w\d\-_]+)/([\w\d\-_]+)'] = 'product/category_slug/vi/$2/$1';
+	$route['en/([\w\d\-_]+)/([\w\d\-_]+)'] = 'product/category_slug/en/$2/$1';
+	$route['([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)'] = 'product/category_slug/vi/$3/$2/$1';
+	$route['en/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)'] = 'product/category_slug/en/$3/$2/$1';
+	$route['([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)'] = 'product/category_slug/vi/$4/$3/$2/$1';
+	$route['en/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)'] = 'product/category_slug/en/$4/$3/$2/$1';
+	# category without id with html
+	$route['([\w\d\-_]+)\.html'] = 'product/category_slug/vi/$1';
+	$route['en/([\w\d\-_]+)\.html'] = 'product/category_slug/en/$1';
+	$route['([\w\d\-_]+)/([\w\d\-_]+)\.html'] = 'product/category_slug/vi/$2/$1';
+	$route['en/([\w\d\-_]+)/([\w\d\-_]+)\.html'] = 'product/category_slug/en/$2/$1';
+	$route['([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)\.html'] = 'product/category_slug/vi/$3/$2/$1';
+	$route['en/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)\.html'] = 'product/category_slug/en/$3/$2/$1';
+	$route['([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)\.html'] = 'product/category_slug/vi/$4/$3/$2/$1';
+	$route['en/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)/([\w\d\-_]+)\.html'] = 'product/category_slug/en/$4/$3/$2/$1';
+	# short with html
+	$route['[\w\d\-_]+\-cp(:num).html'] = 'product/category/vi/$1';
+	$route['en/[\w\d\-_]+\-cp(:num).html'] = 'product/category/en/$1';
+	$route['[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num).html'] = 'product/category/vi/$2/$1';
+	$route['en/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num).html'] = 'product/category/en/$2/$1';
+	$route['[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num).html'] = 'product/category/vi/$3/$2/$1';
+	$route['en/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num).html'] = 'product/category/en/$3/$2/$1';
+	$route['[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num).html'] = 'product/category/vi/$4/$3/$2/$1';
+	$route['en/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num)/[\w\d\-_]+\-cp(:num).html'] = 'product/category/en/$4/$3/$2/$1';
+	
 }
