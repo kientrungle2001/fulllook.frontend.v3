c87e58abb5a6e881f1d0bad0ce4a025ec026f24c
Le Trung Kien <kienlt@smartosc.com>
2020-07-13 19:29:54 +0700

    wp

--- application/controllers/crawler/Get.php
+++ application/controllers/crawler/Get.php
@@ -9,9 +9,23 @@ class Get extends MY_Controller {
     $this->load->model('wprest');
     $this->wprest->setCurl($this->curl);
     $this->wprest->setEndpoint('http://gsmwp.com/wp-json/wp/v2');
+    $post1 = $this->wprest->get('/posts', [
+      // '_fields' => 'id,slug,title,content,excerpt,meta,author'
+      'categories' => [2],
+      'slug' => 'hello-world'
+    ]);
     echo '<pre>';
-    echo json_encode ($this->wprest->get('/posts/1', [
-      '_fields' => 'id,slug,title,content,excerpt,meta,author'
-    ]), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_LINE_TERMINATORS);
+    // echo var_export($post1, true);
+    // echo $post1['title']['rendered'];
+    $teachers = $this->wprest->get('/teachers', [
+      'filter' => [
+        'metaquery' => [
+          [
+            
+          ]
+        ]
+      ]
+    ]);
+    echo var_export($teachers, true);
   }
 }
