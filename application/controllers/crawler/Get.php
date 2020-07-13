287e554a6437700d897c6900e0bc19d55b466071
Le Trung Kien <kientrungle2001@gmail.com>
2020-07-13 23:09:09 +0700

    meta query

--- application/controllers/crawler/Get.php
+++ application/controllers/crawler/Get.php
@@ -8,24 +8,23 @@ class Get extends MY_Controller {
     $this->load->library('curl');
     $this->load->model('wprest');
     $this->wprest->setCurl($this->curl);
-    $this->wprest->setEndpoint('http://gsmwp.com/wp-json/wp/v2');
+    $this->wprest->setEndpoint('https://mobo.com.vn/_pql/wp-json/wp/v2');
     $post1 = $this->wprest->get('/posts', [
-      // '_fields' => 'id,slug,title,content,excerpt,meta,author'
-      'categories' => [2],
-      'slug' => 'hello-world'
+      '_fields' => 'id,slug,title,content,excerpt,meta',
+      'categories' => [2]
     ]);
     echo '<pre>';
     // echo var_export($post1, true);
     // echo $post1['title']['rendered'];
+	
     $teachers = $this->wprest->get('/teachers', [
-      'filter' => [
-        'metaquery' => [
-          [
-            
-          ]
-        ]
-      ]
-    ]);
+		'_fields' => 'teacher_name,age',
+		'filter' => [
+			'meta_key' => 'age',
+			'meta_value' => 36,
+			'meta_compare' => '>'
+		]
+	]);
     echo var_export($teachers, true);
   }
 }
