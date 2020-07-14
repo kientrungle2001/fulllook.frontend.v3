db176bee46c015fc55cf8005ce6f7558b0012e75
Le Trung Kien <kienlt@smartosc.com>
2020-07-14 10:50:16 +0700

    wprest

--- application/controllers/crawler/Get.php
+++ application/controllers/crawler/Get.php
@@ -8,7 +8,8 @@ class Get extends MY_Controller {
     $this->load->library('curl');
     $this->load->model('wprest');
     $this->wprest->setCurl($this->curl);
-    $this->wprest->setEndpoint('https://mobo.com.vn/_pql/wp-json/wp/v2');
+    // $this->wprest->setEndpoint('https://mobo.com.vn/_pql/wp-json/wp/v2');
+		$this->wprest->setEndpoint('http://gsmwp.com/wp-json/wp/v2');
     $post1 = $this->wprest->get('/posts', [
       '_fields' => 'id,slug,title,content,excerpt,meta',
       'categories' => [2]
@@ -16,13 +17,22 @@ class Get extends MY_Controller {
     echo '<pre>';
     // echo var_export($post1, true);
     // echo $post1['title']['rendered'];
-	
+
     $teachers = $this->wprest->get('/teachers', [
-		'_fields' => 'teacher_name,age',
+		'_fields' => 'teacher_name,age,courses',
 		'filter' => [
-			'meta_key' => 'age',
-			'meta_value' => 36,
-			'meta_compare' => '>'
+			'meta_query' => [
+				[
+					'key' => 'age',
+					'value' => 30,
+					'compare' => '>'
+				],
+				[
+					'key' => 'teacher_name',
+					'value' => 'kien',
+					'compare' => 'like'
+				]
+			]
 		]
 	]);
     echo var_export($teachers, true);
